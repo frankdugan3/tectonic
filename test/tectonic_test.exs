535c82d6f5f406042c22cad8b139f3fcdbc13b93
Frank Dugan III <frank3@duganator.com>
2024-10-27 23:36:20 -0500

    tidbit: cleanup

--- test/tectonic_test.exs
+++ test/tectonic_test.exs
@@ -20,24 +20,6 @@ defmodule TectonicTest do
            end) =~ @version
   end
 
-  test "updates on install" do
-    Application.put_env(:tectonic, :version, "0.14.1")
-    Mix.Task.rerun("tectonic.install", ["--if-missing"])
-
-    assert ExUnit.CaptureIO.capture_io(fn ->
-             # This hack is because the previous release no longer runs due to old SSL (Arch BTW).
-             assert Tectonic.run(:another, []) == 127
-           end) =~ "libssl.so.1.1"
-
-    Application.delete_env(:tectonic, :version)
-
-    Mix.Task.rerun("tectonic.install", ["--if-missing"])
-
-    assert ExUnit.CaptureIO.capture_io(fn ->
-             assert Tectonic.run(:default, ["--version"]) == 0
-           end) =~ @version
-  end
-
   test "installs with custom URL" do
     assert :ok = Mix.Task.rerun("tectonic.install", [Tectonic.default_base_url()])
   end
